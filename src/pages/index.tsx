4d4ccf21522aba3d71192f9479436b4a448f4083
unknown <doniaskima344@gmail.com>
2023-06-26 20:01:29 +0100

    feat(clean up the code=

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-import { Layout, StackedSection } from "../components/Layout"
-import Bio from "../components/Hero"
-import { ChakraProvider, Flex, Grid, extendTheme } from "@chakra-ui/react";
-import ExternalLink from "../components/ExternalLink"
-import { RiGithubFill, RiRssFill } from "react-icons/ri"
-import { SectionHeader } from "../components/SectionHeader"
-import { PostList } from "../components/PostList"
+import React from "react";
+import { ChakraProvider, extendTheme, Flex } from "@chakra-ui/react";
+import { RiGithubFill, RiRssFill } from "react-icons/ri";
+import type { HeadFC, PageProps } from "gatsby";
+import { Layout, StackedSection } from "../components/Layout";
+import Bio from "../components/Hero";
+import ExternalLink from "../components/ExternalLink";
+import { SectionHeader } from "../components/SectionHeader";
+import { PostList } from "../components/PostList";
 
 const theme = extendTheme({
   fonts: {
-    heading: "Inter",  
+    heading: "Inter",
   },
   fontSizes: {
     xl: "3rem",
@@ -21,15 +21,14 @@ const theme = extendTheme({
     "6xl": "5.5rem",
     "7xl": "6rem",
     "8xl": "6.5rem",
-    "9xl": "7rem",  
+    "9xl": "7rem",
   },
 });
-
 
 const IndexPage: React.FC<PageProps> = () => {
   const post = {
     frontmatter: {
-      title: "Example Post Talking about Docker ",
+      title: "Example Post Talking about Docker",
       description: "This is an example post",
       rawDate: "2023-06-26T12:00:00Z",
     },
@@ -40,19 +39,18 @@ const IndexPage: React.FC<PageProps> = () => {
       },
     },
   };
-  return (
-<>
 
-<ChakraProvider theme={theme}>
-   <Layout
-      display="flex"
-      flexDirection="column"
-      maxWidth="1200px"
-      margin={["0 auto", null, "5% auto"]}
-      gap={{ base: 6, md: 10, lg: 24 }}
-    >
-    <Bio/>
-    <StackedSection gridArea="blog" flex={1} alignItems="center" >
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout
+        display="flex"
+        flexDirection="column"
+        maxWidth="1200px"
+        margin={["0 auto", null, "5% auto"]}
+        gap={{ base: 6, md: 10, lg: 24 }}
+      >
+        <Bio />
+        <StackedSection gridArea="blog" flex={1} alignItems="center">
           <Flex
             flexFlow="row"
             justifyContent="space-between"
@@ -61,14 +59,14 @@ const IndexPage: React.FC<PageProps> = () => {
             mt={16}
           >
             <Flex alignItems="center">
-              <SectionHeader> 12 Posts</SectionHeader>
+              <SectionHeader>12 Posts</SectionHeader>
               <ExternalLink
                 color="text.300"
                 ml={2}
                 href="/rss.xml"
                 aria-label="Go to RSS feed"
               >
-                <RiRssFill size={15} />  
+                <RiRssFill size={15} />
               </ExternalLink>
             </Flex>
             <Flex alignItems="center">
@@ -82,7 +80,6 @@ const IndexPage: React.FC<PageProps> = () => {
               </ExternalLink>
               <RiGithubFill size={18} />
             </Flex>
-          
           </Flex>
           <Flex
             flexFlow="column"
@@ -91,24 +88,15 @@ const IndexPage: React.FC<PageProps> = () => {
             width="80%"
             mt={8}
           >
-          <PostList node={post} date={new Date(post.frontmatter.rawDate)} />
-          <PostList node={post} date={new Date(post.frontmatter.rawDate)} />
-
+            <PostList node={post} date={new Date(post.frontmatter.rawDate)} />
+            <PostList node={post} date={new Date(post.frontmatter.rawDate)} />
           </Flex>
-           
-       
         </StackedSection>
-  </Layout>
- 
-</ChakraProvider>
+      </Layout>
+    </ChakraProvider>
+  );
+};
 
- 
-</>
- 
+export default IndexPage;
 
-  )
-}
-
-export default IndexPage
-
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
